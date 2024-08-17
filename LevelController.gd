1f2c141cdcd1d7b01e1ee08253196293594f2529
Vincent Miller <vmiller0725@gmail.com>
2024-08-17 13:25:30 -0700

    Level updates

--- LevelController.gd
+++ LevelController.gd
@@ -5,31 +5,28 @@ extends Node3D
 @onready var player_canvas: PlayerCanvas = $PlayerCanvas
 
 @export var navigator_spawn_points: Array[NavigatorSpawn]
-@export var player_scene: PackedScene
-@export var Map: EditableMap
-
-var player: PlayerController
+@export var player: PlayerController
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_birth_player() 
 	
 func _birth_player():
-	player = player_scene.instantiate()
-	add_child(player)
+	if player.get_parent() != self:
+		player.get_parent().remove_child(player)
+		add_child(player)
 	player.position = navigator_spawn_points[0].position
 	player.player_canvas = player_canvas
-	player.Map = Map
 	player.CameraNode.make_current()
 	player.connect("begin_trial",_on_player_begin_trial)
 
 func _on_player_begin_trial(image: Image) -> void:
-	player.queue_free()
+	remove_child(player)
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	player_canvas.hide()
 	playback_canvas.activate()
 	
 	MapGridHandler.ParseImage(image)
-	%TextureRect.texture = ImageTexture.create_from_image(image)
+	#%TextureRect.texture = ImageTexture.create_from_image(image)
 	
 	for nsp in navigator_spawn_points: nsp.spawn_navigator() 
