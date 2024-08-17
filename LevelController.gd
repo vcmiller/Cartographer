937454b35a80e1ae116fa609bd3eeee59b253ca8
Kate Briggs <katebriggs.dev@gmail.com>
2024-08-17 12:32:09 -0500

    Actually support multiple navs

--- LevelController.gd
+++ LevelController.gd
@@ -1,11 +1,9 @@
 extends Node3D
 
-@onready var navigator_spawn: Marker3D = $NavigatorSpawn
 @onready var spectator_camera: Camera3D = $SpectatorCamera
 @onready var player: CharacterBody3D = $Player
-@onready var target: Marker3D = $Target
 
-@export var navigator: PackedScene = load("res://navigator.tscn")
+@export var navigator_spawn_points: Array[NavigatorSpawn]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,8 +23,4 @@ func _on_player_begin_trial(image: Image) -> void:
 	MapGridHandler.ParseImage(image)
 	%TextureRect.texture = ImageTexture.create_from_image(image)
 	
-	var navigator_inst = navigator.instantiate()
-	add_child(navigator_inst)
-	navigator_inst.target = target
-	navigator_inst.position = navigator_spawn.position
-	pass # Replace with function body.
+	for nsp in navigator_spawn_points: nsp.spawn_navigator() 
---
+++ navigator_spawn.gd
@@ -0,0 +1,11 @@
+extends Marker3D
+class_name NavigatorSpawn
+
+@export var target: Node3D
+@export var navigator_scene: PackedScene
+
+func spawn_navigator(): 
+	var navigator_inst = navigator_scene.instantiate()
+	get_parent().add_child(navigator_inst)
+	navigator_inst.target = target
+	navigator_inst.position = position
