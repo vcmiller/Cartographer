7bdeaad87ecfcbce7680eb192a6b580551f5e5ec
Vincent Miller <vmiller0725@gmail.com>
2024-08-17 16:54:52 -0700

    split screen

--- navigator_spawn.gd
+++ navigator_spawn.gd
@@ -4,8 +4,9 @@ class_name NavigatorSpawn
 @export var target: Node3D
 @export var navigator_scene: PackedScene
 
-func spawn_navigator(): 
+func spawn_navigator() -> Navigator: 
 	var navigator_inst = navigator_scene.instantiate()
 	get_parent().add_child(navigator_inst)
 	navigator_inst.target = target
 	navigator_inst.position = position
+	return navigator_inst
--- playback_canvas.gd
+++ playback_canvas.gd
@@ -4,11 +4,65 @@ class_name PlaybackCanvas
 @onready var fuck_label: Label = $FuckLabel
 
 @export var level_controller: LevelController
+@export var ViewportsLeft: CanvasItem
+@export var ViewportsRight: CanvasItem
+@export var ViewportParentTL: SubViewportContainer
+@export var ViewportParentBL: SubViewportContainer
+@export var ViewportParentTR: SubViewportContainer
+@export var ViewportParentBR: SubViewportContainer
+@export var CameraTL: Camera3D
+@export var CameraTR: Camera3D
+@export var CameraBL: Camera3D
+@export var CameraBR: Camera3D
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("map_submit") and is_visible(): 
 		level_controller.SaveMap()
 		get_tree().reload_current_scene()
+		
+func _ready() -> void:
+	ViewportsLeft.hide()
+	ViewportsRight.hide()
+		
+func update_viewports(navigators: Array[Navigator]):
+	var count = len(navigators)
+	if count == 0 or count > 4:
+		printerr("must have at least one and at most 4 navigators.")
+	
+	var cameras: Array[Camera3D]
+	if count == 1:
+		ViewportsLeft.show()
+		ViewportsRight.hide()
+		ViewportParentTL.show()
+		ViewportParentBL.hide()
+		cameras = [CameraTL]
+	elif count == 2:
+		ViewportsLeft.show()
+		ViewportsRight.show()
+		ViewportParentTL.show()
+		ViewportParentBL.hide()
+		ViewportParentTR.show()
+		ViewportParentBR.hide()
+		cameras = [CameraTL, CameraTR]
+	elif count == 3:
+		ViewportsLeft.show()
+		ViewportsRight.show()
+		ViewportParentTL.show()
+		ViewportParentBL.show()
+		ViewportParentTR.show()
+		ViewportParentBR.hide()
+		cameras = [CameraTL, CameraBL, CameraTR]
+	else:
+		ViewportsLeft.show()
+		ViewportsRight.show()
+		ViewportParentTL.show()
+		ViewportParentBL.show()
+		ViewportParentTR.show()
+		ViewportParentBR.show()
+		cameras = [CameraTL, CameraBL, CameraTR, CameraBR]
+		
+	for i in range(count):
+		navigators[i].remote.remote_path = cameras[i].get_path()
 
 func activate():
 	show()
